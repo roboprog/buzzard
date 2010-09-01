696799cb23bb1fb9a6786cf478e738470ba35fdb
Rob Anderson <roboprog@yahoo.com>
2010-09-01 13:19:05 -0700

    add desc / LGPL notice to .c files

--- bzrt/src/bzrt_alloc.c
+++ bzrt/src/bzrt_alloc.c
@@ -16,6 +16,27 @@
  *
  * $Id: $
  */
+/*
+    buzzard:  blaze runtime (so far, just a simple memory management library)
+
+    Copyright (C) 2010, Robin R Anderson
+    roboprog@yahoo.com
+    PO 1608
+    Shingle Springs, CA 95682
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <malloc.h>
 #include <assert.h>
